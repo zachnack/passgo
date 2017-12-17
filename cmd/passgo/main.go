0ebd4331ac36dc11ff227f1d8564f10068f1cd68
Zachary Neal <zachary.neal@icloud.com>
2017-12-16 18:53:57 -0700

    Add prompt function

--- cmd/passgo/main.go
+++ cmd/passgo/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"os"
+	"path"
+
+	"github.com/zachneal/passgo"
+	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/ssh/terminal"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -11,17 +16,45 @@ func main() {
 	app.Usage = "manage your passwords"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "keyring",
-			Value: "./keyring.skr",
-			Usage: "path to pgp keyring",
+			Name:   "keyring",
+			Value:  "./keyring.skr",
+			Usage:  "path to pgp keyring",
 			EnvVar: "PASSGO_KEYRING",
 		},
-		cli.StringFlag {
-			Name: "store",
-			Value: "./password-store",
-			Usage: "path to password store folder",
+		cli.StringFlag{
+			Name:   "store",
+			Value:  "./password-store",
+			Usage:  "path to password store folder",
 			EnvVar: "PASSGO_STORE",
 		},
 	}
 	app.Run(os.Args)
+}
+
+func prompt(key []openpgp.Key) error {
+	pw, err := terminal.ReadPassword(0)
+	if err != nil {
+		return err
+	}
+
+	errcheck = func(e error) {
+		if err != nil {
+			err = e
+		}
+	}
+
+	for _, k := range key {
+		errcheck(k.PrivateKey.Decrypt(pw))
+	}
+	return err
+}
+
+func read(c *cli.Context) error {
+	keyringPath := path.Clean(c.String("keyring"))
+	storePath := path.Clean(c.String("store"))
+	keys, err := passgo.ReadKeyRing(keyringPath)
+	if err != nil {
+		return err
+	}
+
 }
